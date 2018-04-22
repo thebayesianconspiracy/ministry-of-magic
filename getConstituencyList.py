8a8feb413b48ad975cd4e4085dc448c5cd5045a7
Soumyadeep Mukherjee <soumyadeep.mukherjee@appdynamics.com>
2018-04-22 17:54:55 +0530

    Mongod push fixed

--- getConstituencyList.py
+++ getConstituencyList.py
@@ -28,6 +28,7 @@ def find_between(s, start, end):
 state_elections_url = sys.argv[1]
 full_url = "http://myneta.info/" + state_elections_url + "/"
 
+
 r = requests.get(full_url)
 soup = BeautifulSoup(r.text, "html.parser")
 districtHTML =  soup.find_all("h5", class_="title")
@@ -42,14 +43,19 @@ constituencyHTML = soup.find_all("div", class_="items")
 constituencyList = []
 districtId = -1
 
-client = MongoClient("mongodb://soumyadeep:thebayesianconpiracy@ds135916.mlab.com:35916/ministryofmagic")
-db = client.get_database()
+connection = MongoClient("ds135916.mlab.com", 35916)
+db = connection["ministryofmagic"]
+db.authenticate("soumyadeep", sys.argv[2])
+#print(db.collection_names())
+
 
 if ("constituencies" in db.collection_names()):
-    break
+    pass
 else:
     db.create_collection("constituencies")
-    
+
+constituency_collection = db.constituencies
+
 for element in constituencyHTML:
     stringList = element.get_text().strip().split(':')
     if (len(stringList) < 2):
@@ -59,14 +65,16 @@ for element in constituencyHTML:
     constituencyId = find_between(str(element), "constituency_id=", '"')
     single_constituency = constituency(stringList[1],districtList[districtId] , constituencyId, 0)
     constituencyList.append(single_constituency)
-#    constituencyList.append(element.a.get_text().strip())
-
+    constituency_collection.insert_one(single_constituency.__dict__)
+    print(single_constituency)
+    
 
 #for i in range(len(constituencyList)):
 #    print(constituencyList[i])
 #print(districtList)
 
-frozen = jsonpickle.encode(constituencyList)
+#frozen = jsonpickle.encode(constituencyList)
 
-print(frozen)
+#print(frozen)
+
 
